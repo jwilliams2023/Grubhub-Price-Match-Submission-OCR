08443f93c3a50c981b0aee0d22c04c36dccf4fcc
jwilliams2023 <130696072+jwilliams2023@users.noreply.github.com>
2024-01-30 16:41:37 -0600

    new

--- main.py
+++ main.py
@@ -19,67 +19,70 @@ download_dir_files = [f for f in os.listdir(download_dir) if os.path.isfile(os.p
 last_download_name = max(download_dir_files, key=lambda x: os.path.getmtime(os.path.join(download_dir, x))).lower()
 
 total = get_total_price(last_download_name, download_dir, path_to_tesseract)
+if float(total) > 0.0:
 
-# Initialize Web Driver
-options = webdriver.ChromeOptions()
-options.add_argument("--user-data-dir=C:/Users/Joseph/AppData/Local/Google/Chrome/User Data")
+    # Initialize Web Driver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=C:/Users/Joseph/AppData/Local/Google/Chrome/User Data")
 
-driver = webdriver.Chrome(service=Service(r"C:\Users\Joseph\chromedriver-win64\chromedriver.exe"), options=options)
+    driver = webdriver.Chrome(service=Service(r"C:\Users\Joseph\chromedriver-win64\chromedriver.exe"), options=options)
 
-driver.get('https://www.grubhub.com/account/history?'
-           'pageNum=1&pageSize=20&facet=scheduled%3Afalse&facet=orderType%3AALL&sorts=default')
+    driver.get('https://www.grubhub.com/account/history?'
+               'pageNum=1&pageSize=20&facet=scheduled%3Afalse&facet=orderType%3AALL&sorts=default')
 
-wait = WebDriverWait(driver, 20)
+    wait = WebDriverWait(driver, 20)
 
 
-# Run Automation
-button = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/ghs-site-container/span/div/div[3]/div/span/'
-                                                              'div/ghs-router-outlet/div/div/div[2]/span/span/div/span/'
-                                                              'div[2]/div/div[2]/div/div[1]/div/span/div/div[2]/div[5]/'
-                                                              'div/button')))
-button.click()
-button = wait.until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="receipt-body"]/div[4]/div/div[3]/div/div/div[2]/div[2]/a')))
-button.click()
-button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="safeHtmlWrapper1"]/span')))
-button.click()
-button = wait.until(EC.presence_of_element_located(
-    (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/div[2]/span[2]/button')))
-button.click()
-button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="OTHER"]')))
-button.click()
-button = wait.until(EC.presence_of_element_located(
-    (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/form/div[2]/span/button')))
-button.click()
+    # Run Automation
+    button = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/ghs-site-container/span/div/div[3]/div/span/'
+                                                                  'div/ghs-router-outlet/div/div/div[2]/span/span/div/span/'
+                                                                  'div[2]/div/div[2]/div/div[1]/div/span/div/div[2]/div[5]/'
+                                                                  'div/button')))
+    button.click()
+    button = wait.until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="receipt-body"]/div[4]/div/div[3]/div/div/div[2]/div[2]/a')))
+    button.click()
+    button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="safeHtmlWrapper1"]/span')))
+    button.click()
+    button = wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/div[2]/span[2]/button')))
+    button.click()
+    button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="OTHER"]')))
+    button.click()
+    button = wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/form/div[2]/span/button')))
+    button.click()
 
-textbox = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="total"]')))
-textbox.send_keys(total)
-t.sleep(2)
+    textbox = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="total"]')))
+    textbox.send_keys(total)
+    t.sleep(2)
 
-button = wait.until(EC.presence_of_element_located(
-    (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/form/div[2]/span/button')))
-button.click()
+    button = wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/form/div[2]/span/button')))
+    button.click()
 
-# Open the file explorer dialog
-file_input = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/'
-                                                                    'span/div/div/div/div/form/div[1]/div/div[3]/div/'
-                                                                    'div/label/span')))
-file_input.click()
+    # Open the file explorer dialog
+    file_input = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/'
+                                                                        'span/div/div/div/div/form/div[1]/div/div[3]/div/'
+                                                                        'div/label/span')))
+    file_input.click()
 
-t.sleep(1)
-# Type the file path into the file name field
-pyautogui.write(last_download_name)
+    t.sleep(1)
+    # Type the file path into the file name field
+    pyautogui.write(last_download_name)
 
-# Press enter to upload the file
-pyautogui.press('enter')
+    # Press enter to upload the file
+    pyautogui.press('enter')
 
-t.sleep(8)
+    t.sleep(8)
 
-# submit button DO NOT uncommment
-# button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/form/div[2]/span/button')))
-# button.click()
+    # submit button DO NOT uncommment
+    # button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/div/div/span/div/div/div/div/form/div[2]/span/button')))
+    # button.click()
 
-t.sleep(25)
+    t.sleep(25)
 
+else:
+    raise ValueError("No total found")
 
 #make definitions, add to github, add fucntion calls
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import os
-from PIL import Image
-from PIL import ImageOps
+from PIL import Image, ImageOps, ImageEnhance
 from pytesseract import pytesseract
 
 
@@ -10,10 +9,33 @@ def get_total_price(last_download_name, download_dir, path_to_tesseract):
 
     if last_download_name.endswith(('.png', '.jpg', '.jpeg')):
         image_path = os.path.join(download_dir, last_download_name)
-        image_file = Image.open(image_path).convert('1', dither=Image.NONE)
 
-        inverted_image = ImageOps.invert(image_file)
+        # original image
+        image_file = Image.open(image_path)
+
+        # Resize image to 50% bigger
+        width, height = image_file.size
+        new_width = int(width // 3)
+        new_height = int(height // 3)
+        resized_image = image_file.resize((new_width, new_height))
+
+        # contrast improved image
+        contrast_filter = ImageEnhance.Contrast(resized_image)
+        contrast_image_file = contrast_filter.enhance(2)
+
+        # convert the contrasted image to black and white values
+        bw_image_file = contrast_image_file.convert('1', dither=Image.NONE)
+
+        # create a negative of the image
+        inverted_image = ImageOps.invert(bw_image_file)
+
+        # Update image_file to the final processed image
+        image_file = inverted_image
+
+        # show images
         image_file.show()
+        contrast_image_file.show()
+        bw_image_file.show()
         inverted_image.show()
 
         image_text_list = pytesseract.image_to_string(image_file, config=tesseract_config).split()
@@ -27,46 +49,35 @@ def get_total_price(last_download_name, download_dir, path_to_tesseract):
         image_text_set = set(image_text_list_processed)
         print(image_text_set)
 
+
         # If Tesseract can find the total in the image
         if "total" in image_text_set:
             print("Total Found")
             image_prices = []
-            for elem in image_text_list:
+            for elem in image_text_list_processed:
                 if elem == "$":
                     continue
                 elif "$" in set(elem):
-                    image_prices.append(float(elem.strip("$")))
+                    price = elem.strip("$")
+                    image_prices.append(float(price))
                 elif len(elem) == 4 and elem[1] == ".":
                     if elem.replace('.', '', 1).isdigit():
                         image_prices.append(float(elem))
 
             max_price = 30.0
             filtered_image_prices = [k for k in image_prices if k <= max_price]
+
             if len(filtered_image_prices) > 0:
                 total = sorted(filtered_image_prices)[-1]
             else:
                 raise Exception("No prices found")
 
-            print("Prices Found: ", image_prices)
+            print(f'Price Found: {filtered_image_prices}')
             print("Total: ", format(total, '.2f'))
 
         elif "checkout" in image_text_set:
-            total = float(image_text_list[image_text_list_lower.index("checkout") + 1].strip("$"))
+            total = float(image_text_list[image_text_list_processed.index("checkout") + 1].strip("$"))
 
-        # Code to check for subtotal and tax
-
-        # elif "subtotal" in image_text_set:
-        #     subtotal = float(image_text_list[image_text_list_lower.index("subtotal") + 1].strip("$"))
-        # else:
-        #     raise Exception("No subtotal found in Image")
-        #
-        # elif "tax" in image_text_set:
-        #     tax = float(image_text_list[image_text_list_lower.index("tax") + 1].strip("$"))
-        # else:
-        #     raise Exception("No tax found in Image")
-        #
-        # total = subtotal + tax
-        #
         else:
             total = 0.0
             print("ERROR: No Total Price Found in Image")
@@ -75,3 +86,27 @@ def get_total_price(last_download_name, download_dir, path_to_tesseract):
         raise Exception("No Image Found in Directory")
 
     return format(total, '.2f')
+
+
+def process_tokens(strings_list):
+    punctuation = set("-?!,:;&#%*()_/")
+    output_list = []
+
+    for token in strings_list:
+        token = token.lower()
+        for char in token:
+            if char in punctuation:
+                token = token.replace(char, '')
+
+        output_list.append(token)
+
+    return output_list
+
+# def resize_img(img, scale):
+#
+#     width = int(img.shape[1] * scale / 100)
+#     height = int(img.shape[0] * scale / 100)
+#
+#     dsize = (width, height)
+#
+#     return cv2.resize(img, dsize=dsize)
